b1fe2a68e13876f235c4eabd9f47ffbe2a306cc9
NikiforT <nikifor.tatarkin@yandex.ru>
2019-11-09 18:06:59 +0300

    Сделал все в соответствии с ТЗ. ТОчно не понял, нужно ли делать это для активити настроек :)

---
+++ WeatherApp/app/src/main/java/nikifor/tatarkin/weatherapp/MainActivity.java
@@ -0,0 +1,70 @@
+package nikifor.tatarkin.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonSetting = findViewById(R.id.settingButton);
+        buttonSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Setting.class);
+                startActivity(intent);
+                ((TextView) findViewById(R.id.idCity)).setText(SettingModel.getInstance().getCityName());
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+        Toast.makeText(this, TAG +": started", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+        Toast.makeText(this, TAG +": resumed", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+        Toast.makeText(this, TAG +": paused", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+        Toast.makeText(this, TAG +": stopped", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+        Toast.makeText(this, TAG +": destroyed", Toast.LENGTH_SHORT).show();
+    }
+}
+
+
+
+
